6bd636c70cc364b7f800383a0af3801d1900ce3a
DianaStarostyak <d.starostyak@ya.ru>
2024-04-12 18:24:31 +0300

    add Task4-Lesson2

---
+++ Lesson2-2/Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача разными циклами
+// Цикл while
+
+int n = 5;
+int[] array = { 2000, 456, 7563, 769, 1000 };
+int i = 0;
+int max = array[0];
+
+while(i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+//Цикл for
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+// Цикл foreach
+
+max = array[0];
+foreach(int e in array)
+{
+    if (array[e] > max)
+    {
+        max = array[e];
+    }
+}
